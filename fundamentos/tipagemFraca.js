881ca2729f7ffa7222950148cdc05a257e7d2175
Hugo Morais <hugodanielmorais@gmail.com>
2018-12-03 23:12:14 +0000

    Number(function), Variables, Calculation types

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,11 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.232323
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evitar escolher nomes genericos e siglas
+
+
