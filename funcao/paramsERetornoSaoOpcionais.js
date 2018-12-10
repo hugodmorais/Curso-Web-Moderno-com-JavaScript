8694442fefc8d6a33798b422367619ebd9066a28
Hugo Morais <hugodanielmorais@gmail.com>
2018-12-10 22:11:09 +0000

    Functions(arguments)

---
+++ funcao/paramsERetornoSaoOpcionais.js
@@ -0,0 +1,10 @@
+function area(largura, altura) {
+    const area = largura * altura
+    if (area > 20) {
+        console.log(`Valor acima do permitido: ${area} m2.`)
+    } else {
+        return area
+    }
+}
+
+area(1, 9)
