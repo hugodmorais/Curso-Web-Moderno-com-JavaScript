7be207c476adbf95d9c0f435d4d9e631dfd40eab
Hugo Morais <hugodanielmorais@gmail.com>
2018-12-13 22:29:51 +0000

    OO

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!!
+
+// PROCEDURAL
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,// Atributos
+    valor2,
+    valor3,
+    processamento() {// Funções
+        // ...
+    }
+}
+
+objeto.processamento()// Foco passou a ser o objeto
+
+// Principios importantes OO
+// Abstração
+// Encapsulamento
+// Herança (prototype)
+// Polimorfismo
