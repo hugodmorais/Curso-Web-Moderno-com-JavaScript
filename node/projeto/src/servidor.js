5530e4b261dfacaf63680978b08df64ea64cfae6
Hugo Morais <hugodanielmorais@gmail.com>
2018-12-20 21:38:20 +0000

    Middleware, server, Postman

---
+++ node/projeto/src/servidor.js
@@ -0,0 +1,13 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+
+app.get('/produtos', (req, res, next) => {
+    res.send({ nome: 'Notebook', preco: 123.45})// Vai converter para JSON
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor executando na porta ${porta}.`)
+})
+
