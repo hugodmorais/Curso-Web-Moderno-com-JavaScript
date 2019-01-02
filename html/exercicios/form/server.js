683eb8e472d420d1b217cd8477d55a4fc9e88094
Hugo Morais <hugodanielmorais@gmail.com>
2019-01-02 20:54:37 +0000

    Form inputs

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabens</h1>')
+    resp.send('<h1>Parabens, usuário incluido!!</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabens, usuario alterado!!</h1>')
 })
 
 app.listen(3003)
