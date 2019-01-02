e9b86ead9ec0cfe0cd60f981e74b321496a79480
Hugo Morais <hugodanielmorais@gmail.com>
2019-01-02 19:35:50 +0000

    Form, input

---
+++ html/exercicios/form/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1>Parabens</h1>')
+})
+
+app.listen(3003)
+
