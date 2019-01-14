5a6f1d2c2c9961b5a470db1132ac3f67d98f4ae5
Hugo Morais <hugodanielmorais@gmail.com>
2019-01-14 21:07:25 +0000

    Ajax

---
+++ ajax/server.js
@@ -0,0 +1,40 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+const multer = require('multer')
+
+const storage = multer.diskStorage ({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+app.get('/teste', (req, res) => res.send('Ok'))
+
+const upload = multer({ storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluido com sucesso.')
+    })
+})
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.listen(8080, () => console.log('Executando...'))
