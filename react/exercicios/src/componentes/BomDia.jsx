2cd42111c937c075a043120492c2215914a9611a
Hugo Morais <hugodanielmorais@gmail.com>
2019-02-03 11:41:31 +0000

    React components

---
+++ react/exercicios/src/componentes/BomDia.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default props => [
+    <h1 key='h1'>Bom Dia {props.nome}!</h1>,
+    <h2 key='h2'>Até Breve {props.ultimoNome}.</h2>
+]
+    
+        
+
+
+
+//export default props => 
+//    <Fragment>
+//        <h1>Bom Dia {props.nome}!</h1>
+//        <h2>Até Breve {props.ultimoNome}.</h2>
+//    </Fragment> 
---
+++ react/exercicios/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Multi from './componentes/Multiplos'
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Hugo" />
+        <Multi.BoaNoite nome="Morais" />
+    </div>
+, document.getElementById('root'))
+
+
